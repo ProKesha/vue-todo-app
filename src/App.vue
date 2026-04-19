7961dc69dce37b2d4e0c9f640a1b372b97b6022e
Dmytro Popov <lort20178@gmail.com>
2026-04-19 15:07:44 +0300

    set up Vue TodoApp layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,85 @@
-<script setup></script>
+<script setup>
+import todos from './data/todos';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
+  <div class="todoapp">
+    <h1 class="todoapp__title">todos {{ todos.length }}</h1>
 
-<style scoped></style>
+    <div class="todoapp__content">
+      <header class="todoapp__header">
+        <button type="button" class="todoapp__toggle-all active"></button>
+
+        <form>
+          <input
+            type="text"
+            class="todoapp__new-todo"
+            placeholder="What needs to be done?"
+          />
+        </form>
+      </header>
+
+      <section class="todoapp__main">
+        <div
+          v-for="todo of todos"
+          :key="todo.id"
+          class="todo"
+          :class="{ completed: todo.completed }"
+        >
+          <label class="todo__status-label">
+            <input
+              type="checkbox"
+              class="todo__status"
+              :checked="todo.completed"
+            />
+          </label>
+
+          <form v-if="false">
+            <input
+              type="text"
+              class="todo__title-field"
+              placeholder="Empty todo will be deleted"
+            />
+          </form>
+
+          <template v-else>
+            <span class="todo__title">{{ todo.title }}</span>
+            <button type="button" class="todo__remove">×</button>
+          </template>
+
+          <div class="modal overlay" :class="{ 'is-active': false }">
+            <div class="modal-background has-background-white-ter"></div>
+            <div class="loader"></div>
+          </div>
+        </div>
+      </section>
+
+      <footer class="todoapp__footer">
+        <span class="todo-count">3 items left</span>
+
+        <nav class="filter">
+          <a href="#/" class="filter__link selected">All</a>
+          <a href="#/active" class="filter__link">Active</a>
+          <a href="#/completed" class="filter__link">Completed</a>
+        </nav>
+
+        <button type="button" class="todoapp__clear-completed">
+          Clear completed
+        </button>
+      </footer>
+    </div>
+
+    <div class="notification is-danger is-light has-text-weight-normal hidden">
+      <button type="button" class="delete"></button>
+      Unable to load todos
+      <br />
+      Title should not be empty
+      <br />
+      Unable to add a todo
+      <br />
+      Unable to delete a todo
+      <br />
+      Unable to update a todo
+    </div>
+  </div>
+</template>
