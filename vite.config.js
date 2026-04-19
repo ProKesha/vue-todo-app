7a64541f0e789722ef0dcb782b749984fc829d9c
Dmytro Popov <lort20178@gmail.com>
2026-04-19 15:13:54 +0300

    Fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/vue-todo-app/' : '/',
+  base: './',
   plugins: [
     vue(),
   ],
